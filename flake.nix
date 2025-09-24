a1e1da692360f3b0e23743ff0cb0286c754282a7
Sigmanificient <edhyjox@gmail.com>
2025-09-24 02:07:17 +0200

    build(nix): declare android env & dependencies to build an apk

--- flake.nix
+++ flake.nix
@@ -12,7 +12,15 @@
     self, nixpkgs, git-hooks
   }: let
     applySystems = nixpkgs.lib.genAttrs ["x86_64-linux"];
-    forAllSystems = f: applySystems (system: f nixpkgs.legacyPackages.${system});
+    forAllSystems = f: applySystems (system:
+      f (import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
+      })
+    );
   in {
     formatter = forAllSystems (pkgs: pkgs.alejandra);
 
@@ -38,14 +46,48 @@
       }
     );
 
-    devShells = forAllSystems (pkgs: {
+    devShells = forAllSystems (pkgs: let
+      sdkArgs = {
+        includeNDK = false;
+        includeSystemImages = false;
+        includeEmulator = false;
+
+        buildToolsVersions = [ "34.0.0" ];
+        platformVersions = [ "34" "35" ];
+
+        systemImageTypes = [ "google_apis" ];
+        abiVersions = [ "arm64-v8a" "x86_64" ];
+
+        extraLicenses = [
+          "android-sdk-preview-license"
+          "android-googletv-license"
+          "android-sdk-arm-dbt-license"
+          "google-gdk-license"
+          "intel-android-extra-license"
+          "intel-android-sysimage-license"
+          "mips-android-sysimage-license"
+        ];
+      };
+
+      androidComposition = pkgs.androidenv.composeAndroidPackages sdkArgs;
+      androidSdk = androidComposition.androidsdk;
+    in {
       default = pkgs.mkShell {
         inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
+
+        env.ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+
         packages = with pkgs; [
           biome
           nodejs
           pnpm
-        ];
+          jdk
+          gradle
+        ] ++ (with androidComposition; [
+          androidsdk
+          platform-tools
+          build-tools
+        ]);
       };
     });
   };
