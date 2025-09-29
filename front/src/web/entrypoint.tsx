32dea0f7d60d3e3bce1a893fe8c9f6f93c1a7c0b
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-09-29 10:40:40 +0200

    feat(front): add POC to web page

--- front/src/web/entrypoint.tsx
+++ front/src/web/entrypoint.tsx
@@ -1,13 +1,28 @@
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import "@/index.scss";
+
+import { AuthProvider, LoginRequired } from "../auth";
+
+import "./index.scss";
+
+import GraphPage from "../routes/graph";
+import HomePage from "../routes/home";
+import LoginPage from "../routes/login";
+
 
 function WebApp() {
   return (
+  <AuthProvider>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<p>Web</p>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<LoginRequired />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/graph" element={<GraphPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+  </AuthProvider>
   );
 }
 
