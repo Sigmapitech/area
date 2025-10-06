0e148752cd3be28aec02109e7d9f116c5b443626
Sigmanificient <edhyjox@gmail.com>
2025-10-06 10:44:04 +0200

    feat(mobile): add api check guard

--- front/src/mobile/entrypoint.tsx
+++ front/src/mobile/entrypoint.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { AuthProvider, LoginRequired } from "@/auth";
-
 import GraphPage from "@/routes/graph";
 import HomePage from "@/routes/home";
 import LoginPage from "@/routes/login";
+import { CheckAPIConnection } from "./api-guard";
 
 function MobileApp() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<CheckAPIConnection />} />
+
           <Route path="/login" element={<LoginPage />} />
           <Route element={<LoginRequired />}>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
             <Route path="/graph" element={<GraphPage />} />
           </Route>
         </Routes>
