78d1fc859e2b1698f9e9cc2dd7ab0a0de6b33ef6
Ciznia <hosquetgabriel@gmail.com>
2025-10-07 16:18:25 +0200

    test(endpoint): refactor router import to allow skip  on provider if invalid config

--- back/app/api/routes/__init__.py
+++ back/app/api/routes/__init__.py
@@ -1,11 +1,23 @@
+from importlib import import_module
+
 from fastapi import APIRouter
 
-from ...providers.spotify import spotify
 from . import auth, graph, hello
+
+optional_providers = ["spotify.spotify"]
+
+loaded_modules = [auth, graph, hello]
+
+for name in optional_providers:
+    try:
+        mod = import_module(f"...providers.{name}", package=__package__)
+        loaded_modules.append(mod)
+    except Exception as e:
+        print(f"[WARN] Skipping provider '{name}' due to import error: {e}")
 
 routers = []
 
-for mod in (auth, graph, hello, spotify):
+for mod in loaded_modules:
     assert hasattr(
         mod, "router"
     ), f"Module {mod.__name__} is missing 'router' attribute"
@@ -16,5 +28,6 @@ for mod in (auth, graph, hello, spotify):
         f"Registering router from module: {mod.__name__} with prefix: {mod.router.prefix}"
     )
     routers.append(mod.router)
+
 
 __all__ = ("routers",)
