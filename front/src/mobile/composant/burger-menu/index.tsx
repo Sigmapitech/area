a4d7034a4e3f741f5bc40f3cd59f616362887f0f
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-10-07 21:41:11 +0200

    feat(mobile/burger-menu): add base for burger menu

---
+++ front/src/mobile/composant/burger-menu/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./burger.scss";
+
+interface BurgerProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+import { useEffect, type RefObject } from "react";
+
+type EventType = TouchEvent;
+
+export const useOnClickOutside = <T extends HTMLElement>(
+  ref: RefObject<T | null>,
+  handler: (event: EventType) => void
+): void => {
+  useEffect(() => {
+    const listener = (event: EventType) => {
+      const el = ref?.current;
+      if (!el || el.contains(event.target as Node)) return;
+      handler(event);
+    };
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
+
+const Burger: React.FC<BurgerProps> = ({ open, setOpen }) => {
+  return (
+    <button
+      className={`burger ${open ? "open" : ""}`}
+      onClick={() => setOpen(!open)}
+      aria-label="Toggle menu"
+    >
+      <div />
+      <div />
+      <div />
+    </button>
+  );
+};
+
+export default Burger;
--- front/src/mobile/entrypoint.tsx
+++ front/src/mobile/entrypoint.tsx
@@ -4,8 +4,15 @@ import GraphPage from "@/routes/graph";
 import HomePage from "@/routes/home";
 import LoginPage from "@/routes/login";
 import { CheckAPIConnection } from "./api-guard";
+import Burger, { useOnClickOutside } from "@/mobile/composant/burger-menu";
+import { useRef, useState } from "react";
 
 function MobileApp() {
+  const [open, setOpen] = useState(false);
+  const node = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
     <AuthProvider>
       <BrowserRouter>
