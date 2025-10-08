dbf43e512d50173c250d9cc3302777aea5a2f990
Sigmanificient <edhyjox@gmail.com>
2025-10-08 02:54:39 +0200

    refactor(front/layout): cleanup main layout

--- front/src/layouts/main/index.tsx
+++ front/src/layouts/main/index.tsx
@@ -1,22 +1,18 @@
 import AREA from "../../../favicon.svg";
-import home from "@material-design-icons/svg/round/home.svg";
-import folder from "@material-design-icons/svg/round/folder.svg";
-import density_medium from "@material-design-icons/svg/round/density_medium.svg";
-import keyboard_arrow_down from "@material-design-icons/svg/round/keyboard_arrow_down.svg";
 import "./style.scss";
 
+import { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router";
 import { API_BASE_URL } from "@/api_url";
 import { useAuth } from "@/auth";
 
-import { Link, useLocation } from "react-router";
-import { useEffect, useState, type ReactNode } from "react";
-import { Outlet } from "react-router";
-
 export default function MainLayout() {
   const { token } = useAuth();
-  const [userInitial, setUserInitial] = useState("X");
+  const [userInitial, setUserInitial] = useState("?");
 
   useEffect(() => {
+    if (!token) return;
+
     fetch(`${API_BASE_URL}/api/auth/me`, {
       headers: { Authorization: `Bearer ${token}` },
     })
@@ -26,87 +22,25 @@ export default function MainLayout() {
       })
       .catch((e) => {
         console.error(e);
-        setUserInitial("X");
       });
   }, [token]);
 
-  const path = useLocation().pathname.split("/");
-  const titleNodes: ReactNode[] = [
-    <div key={`page`} className="text-wrapper">
-      {path.length > 1 ? path[1] : "Home"}
-    </div>,
-  ];
-  path.forEach((subname, i) => {
-    // Add separator before all but first breadcrumb
-    if (i > 0)
-      titleNodes.push(
-        <div key={`sep-${subname}`} className="text-wrapper">
-          /
-        </div>
-      );
-    titleNodes.push(
-      <div key={`b-${subname}`} className="text-wrapper">
-        {subname}
-      </div>
-    );
-  });
-
   return (
     <div className="main">
-      <div className="top-bar">
-        <div className="top-left-corner">
-          <Link to="/" className="vector-wrapper home-link">
-            <img className="img" alt="Home" src={home} />
-          </Link>
+      <header className="main-bar">
+        <Link to="/" className="home-link">
+          <img src={AREA} height="24px" width="24px" alt="Area" />
+          <span>Area</span>
+        </Link>
+
+        <div className="user-icon">
+          <span className="user-icon-intial">{userInitial}</span>
         </div>
+      </header>
 
-        <div className="top-content">
-          <div className="left">
-            <div className="text-wrapper">-</div>
-
-            <div className="app-title">
-              <div className="vector-wrapper">
-                <img className="img" alt="Area" src={AREA} />
-              </div>
-
-              <div className="text-wrapper">Area</div>
-            </div>
-          </div>
-
-          <div className="center">
-            <div className="vector-wrapper">
-              <img className="img" alt="Folder" src={folder} />
-            </div>
-            {titleNodes}
-
-            <div className="vector-wrapper">
-              <img
-                className="img"
-                alt="Keyboard Arrow Down"
-                src={keyboard_arrow_down}
-              />
-            </div>
-          </div>
-
-          <div className="right">
-            <div className="user-icon">
-              <div className="div">{userInitial}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="center-content">
-        <div className="left-bar">
-          <div className="vector-wrapper">
-            <img className="img" alt="Density Medium" src={density_medium} />
-          </div>
-        </div>
-
-        <div className="main-content">
-          <Outlet />
-        </div>
-      </div>
+      <main className="main-content">
+        <Outlet />
+      </main>
     </div>
   );
 }
