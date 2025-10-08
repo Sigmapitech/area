b0b1e66573644d6d1005eade085ae60f7565cfe4
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-10-08 18:18:15 +0200

    fix(mobile/entrypoint): fix linting errors

--- front/src/mobile/entrypoint.tsx
+++ front/src/mobile/entrypoint.tsx
@@ -1,24 +1,13 @@
-import { useRef, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { AuthProvider, LoginRequired } from "@/auth";
-import Burger, { useOnClickOutside } from "@/mobile/composant/burger-menu";
 import GraphPage from "@/routes/graph";
 import HomePage from "@/routes/home";
 import LoginPage from "@/routes/login";
 import { CheckAPIConnection } from "./api-guard";
 
 function MobileApp() {
-  const [open, setOpen] = useState(false);
-  const node = useRef<HTMLDivElement>(null);
-
-  useOnClickOutside(node, () => setOpen(false));
-
   return (
     <AuthProvider>
-      <Burger open={open} setOpen={setOpen}>
-        <p>test thingie</p>
-        <p>test 2</p>
-      </Burger>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<CheckAPIConnection />} />
