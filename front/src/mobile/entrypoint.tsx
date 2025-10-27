a781c65f6be5e92af4e74a613dcd45fe840ca2b3
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-10-27 09:42:51 +0100

    feat(mobile): add workflow list

--- front/src/mobile/entrypoint.tsx
+++ front/src/mobile/entrypoint.tsx
@@ -4,6 +4,7 @@ import HomePage from "@/routes/home";
 import LoginPage from "@/routes/login";
 import TestSpotifyPage from "@/routes/test-spotify";
 import { CheckAPIConnection } from "./api-guard";
+import WorkflowList from "@/routes/workflow-create";
 
 function MobileApp() {
   return (
@@ -14,6 +15,8 @@ function MobileApp() {
           <Route path="/login" element={<LoginPage />} />
           <Route element={<LoginRequired />}>
             <Route path="/home" element={<HomePage />} />
+            <Route path="/graph" element={<WorkflowList />} />
+            <Route path="/graph/:id" element={<GraphPage />} />
             <Route path="/test-spotify" element={<TestSpotifyPage />} />
           </Route>
         </Routes>
