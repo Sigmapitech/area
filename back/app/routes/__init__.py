cee9de5637227442186be821170fff4a281a8fe9
Sigmanificient <edhyjox@gmail.com>
2025-11-01 14:37:13 +0100

    fix(back): lazify oauth imports

--- back/app/routes/__init__.py
+++ back/app/routes/__init__.py
@@ -1,29 +1,33 @@
+import importlib
+
 from fastapi import APIRouter
 
-from . import (
-    auth,
-    caldav,
-    discord,
-    gmail,
-    hello,
-    spotify,
-    workflow,
-    youtube,
+routers = []
+providers = (
+    "auth",
+    "hello",
+    "workflow",
+    "spotify",
+    "discord",
+    "gmail",
+    "caldav",
+    "youtube",
 )
 
-routers = []
+for mod_name in providers:
+    try:
+        mod = importlib.import_module(f".{mod_name}", __package__)
 
-for mod in (auth, hello, workflow, spotify, discord, gmail, caldav, youtube):
-    assert hasattr(
-        mod, "router"
-    ), f"Module {mod.__name__} is missing 'router' attribute"
-    assert isinstance(
-        mod.router, APIRouter
-    ), f"'router' in module {mod.__name__} is not an APIRouter instance"
-    print(
-        f"Registering router from module: {mod.__name__} with prefix: {mod.router.prefix}"
-    )
-    routers.append(mod.router)
-
+        assert hasattr(mod, "router"), \
+            f"Module {mod.__name__} is missing 'router' attribute"
+        assert isinstance(mod.router, APIRouter), \
+            f"'router' in module {mod.__name__} is not an APIRouter instance"
+        print(
+            "Registering router from module:"
+            f" {mod.__name__} with prefix: {mod.router.prefix}"
+        )
+        routers.append(mod.router)
+    except Exception as e:
+        print(e)
 
 __all__ = ("routers",)
