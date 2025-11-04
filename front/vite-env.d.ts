0a89fc806c7c0a3d8bb520cf1a4c95d83e60dd47
Sigmanificient <edhyjox@gmail.com>
2025-11-04 23:50:03 +0100

    fix(front): setup api url properly

--- front/vite-env.d.ts
+++ front/vite-env.d.ts
@@ -2,4 +2,12 @@
 
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly PROD: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare const __APP_PLATFORM__: "mobile" | "web";
